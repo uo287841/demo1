f5471a51e61797d9520309a7d37e4325b1dc057f
luiss <luiss@LAPTOP-5F2C5SQV.mshome.net>
2023-10-05 21:34:40 +0200

    cambio y borrado

---
+++ src/demo1/Demo2.java
@@ -0,0 +1,11 @@
+package demo1;
+
+public class Demo2 {
+	public static void main (String[] args) {
+		System.out.println("linei 1");
+		System.out.println("linei 1");
+		System.out.println("linei 1");
+		
+	}
+
+}
