a4bc201c0189550fa4d6556c2205c22581c6866b
luiss <luiss@LAPTOP-5F2C5SQV.mshome.net>
2023-10-05 22:14:18 +0200

    cambio3

--- src/demo1/Demo.java
+++ src/demo1/Demo.java
@@ -3,7 +3,7 @@ package demo1;
 public class Demo {
 	public static void main (String[] args) {
 		System.out.println("linei 1");
-		System.out.println("linei 1");
+		System.out.println("cambio 3 mars en confli");
 		System.out.println("linei 1");
 		
 		System.out.println("linea despues de 'new branch'");
