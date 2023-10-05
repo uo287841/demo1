4ee6de3e20dc1ad0ad26a7ed01db6b34b5579f52
luiss <luiss@LAPTOP-5F2C5SQV.mshome.net>
2023-10-05 21:27:23 +0200

    4.1 update

--- src/demo1/Demo.java
+++ src/demo1/Demo.java
@@ -1,5 +1,11 @@
 package demo1;
 
 public class Demo {
+	public static void main (String[] args) {
+		System.out.println("linei 1");
+		System.out.println("linei 1");
+		System.out.println("linei 1");
+		
+	}
 
 }
